33938d3bfcc8aa13ec812bf0ef613568093c7df7
Colby Blaisdell <ccblaisdell@gmail.com>
2010-11-11 13:44:16 -0600

    Empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RapidUpload::Application.config.secret_token = 'c397beda8308aecf1681e6e9490c73bae80de711affcf09a56037be67b84826c0bcd02d2ffc12eebfa7a35c97074e4e71070992cc2ee35abce36f066ea304c34'
